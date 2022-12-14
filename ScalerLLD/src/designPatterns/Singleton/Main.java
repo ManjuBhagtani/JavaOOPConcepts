b0d4a5952dce0216d96b2af2f7c271ed6713981b
Manju Bhagtani <manjubhagtani@gmail.com>
2022-12-14 12:01:32 +0530

    Singleton design pattern demo

---
+++ ScalerLLD/src/designPatterns/Singleton/Main.java
@@ -0,0 +1,9 @@
+package designPatterns.Singleton;
+
+public class Main {
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getObj();
+        Singleton s2 = Singleton.getObj();
+        Singleton s3 = Singleton.getObj();
+    }
+}
---
+++ ScalerLLD/src/designPatterns/Singleton/Singleton.java
@@ -0,0 +1,15 @@
+package designPatterns.Singleton;
+
+public class Singleton{
+    private static Singleton obj;
+    private Singleton(){
+
+    }
+
+    public static Singleton getObj(){
+        if(obj == null){
+            obj = new Singleton();
+        }
+        return obj;
+    }
+}
