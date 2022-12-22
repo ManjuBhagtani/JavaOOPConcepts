98b7acbdc6d49914b7290c498b46b49069209821
Manju Bhagtani <manjubhagtani@gmail.com>
2022-12-22 19:43:53 +0530

    Polymorphism demo from LLD: OOP4

---
+++ ScalerLLD/src/OOP/polymorphism/Main.java
@@ -0,0 +1,31 @@
+package OOP.polymorphism;
+
+import java.sql.SQLOutput;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        List<User> users = new ArrayList<>();
+
+        Student s1 = new Student("Rajiv", "r@email.com", "123");
+        Student s2 = new Student("Srikanth", "s@email.com", "123");
+        Mentor m1 = new Mentor("Krishnasamy", "k@email.com", "123");
+        TA t1 = new TA("Abhay", "a@email.com", "123");
+
+        users.add(s1); //users[0] = s1;
+        users.add(s2); //users[1] = s2;
+        users.add(m1);
+        users.add(t1);
+
+        printNames(users);
+    }
+
+    private static void printNames(List<User> users) {
+        for(int i = 0; i<users.size(); i++){
+            System.out.println(users.get(i).name);
+        }
+    }
+
+
+}
---
+++ ScalerLLD/src/OOP/polymorphism/User.java
@@ -0,0 +1,13 @@
+package OOP.polymorphism;
+
+public class User {
+    public String name;
+    public String email;
+    public String password;
+
+    public User(String uName, String uEmail, String uPassword) {
+        name = uName;
+        email = uEmail;
+        password = uPassword;
+    }
+}
