c5e183c109a1ccc395e600bac78e6c4034d3ec70
Manju Bhagtani <manjubhagtani@gmail.com>
2022-12-22 22:57:53 +0530

    Method overriding demo from LLD: OOP4

---
+++ ScalerLLD/src/OOP/polymorphism/methodOverriding/ICEVehicle.java
@@ -0,0 +1,15 @@
+package OOP.polymorphism.methodOverriding;
+
+public class ICEVehicle extends Vehicle{
+
+    @Override
+    public void startEngine(){
+        super.startEngine();
+        System.out.println("Start ignition and combustion");
+        System.out.println("ICE engine started");
+    }
+
+    public void blowHorn(){
+        System.out.println("Blowing horn in ICEVehicle");
+    }
+}
